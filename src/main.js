8fa5f21a0ea8d24c60c3d5c2ceff6d2b6db2bc51
Aaron Baker <aaronbakeremail@gmail.com>
2020-07-22 18:42:56 +0100

    Add analytics in production

--- src/main.js
+++ src/main.js
@@ -5,6 +5,14 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
+if (process.env.NODE_ENV === 'production') {
+  const VueGtag = require('vue-gtag');
+
+  Vue.use(VueGtag, {
+    config: { id: process.env.VUE_APP_GA_ID },
+  }, router);
+}
+
 new Vue({
   router,
   store,
